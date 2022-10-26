31c614433520173132e91eca47e6a53d8085b974
Qinlan Weng <QWeng@slb.com>
2022-10-26 17:24:01 +0800

    feat: add multiple of 3 logic

--- FizzBuzzTest/FizzBuzzTest.cs
+++ FizzBuzzTest/FizzBuzzTest.cs
@@ -19,5 +19,19 @@ namespace FizzBuzzTest
             //then
             Assert.Equal("1", result);
         }
+
+        [Fact]
+        public void Should_return_Fizz_given_multiple_of_3()
+        {
+            //given
+            GameFizzBuzz game = new GameFizzBuzz();
+            int number = 3;
+
+            //when
+            var result = game.Countoff(number);
+
+            //then
+            Assert.Equal("Fizz", result);
+        }
     }
 }
