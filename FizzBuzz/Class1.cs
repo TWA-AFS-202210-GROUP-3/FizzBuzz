1f20cdf3b805445d1800f2f4fe1c5e46b61275e6
Zejie Yao <ZYao3@slb.com>
2022-10-26 17:18:10 +0800

    feat: add Should_return_normal_num_when_input_normal_num

--- FizzBuzz/Class1.cs
+++ FizzBuzz/Class1.cs
@@ -4,5 +4,9 @@ namespace FizzBuzz
 {
     public class Class1
     {
+        public string FizzBuzzGame(int num)
+        {
+            return num.ToString();
+        }
     }
 }
--- FizzBuzzTest/Class1Test.cs
+++ FizzBuzzTest/Class1Test.cs
@@ -7,12 +7,16 @@ namespace FizzBuzzTest
     public class Class1Test
     {
         [Fact]
-        public void Test1()
+        public void Should_return_normal_num_when_input_normal_num()
         {
+            // given
             Class1 class1 = new Class1();
+            int num = 1;
+            // when
+            string res = class1.FizzBuzzGame(num);
 
-            //then
-            Assert.NotNull(class1);
+            // then
+            Assert.Equal("1", res);
         }
     }
 }
